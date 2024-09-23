8ff81933b3a1f6fdc503f25adb148461cc9a7422
[durvesh wagh] <[durveshwagh11@gmail.com]>
2024-09-23 23:08:24 +0530

    commit

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
 const app = express();
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
 dotenv.config();
-const userRoute = require("./routes/users")
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL)
-    .then(() => console.log("DB Connected"))
-    .catch((err) => {
-        console.log("Error connecting to DB:", err);
-    });
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
-app.get("/api/test", (req, res) => {
-    console.log("test is successful");
-    res.send("Test is successful");  // Send response to client
-});
-
-app.use("/api/users", userRoute)
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running on port ${process.env.PORT || 3000}`);
+  console.log("Backend server is running!");
 });
